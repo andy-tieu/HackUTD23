598c6b6058b61795317c0b037a660ddc31490573
richard-nguyen00 <106111300+richard-nguyen00@users.noreply.github.com>
2023-11-05 11:25:24 +0000

    made strings into floats

--- calculate.js
+++ calculate.js
@@ -1,36 +1,40 @@
 function getValues() {
-    var downPay = document.getElementById("downPay").value;
-    var grossMonthlyIncome = document.getElementById("grossIncome").value;
-    var houseValue = document.getElementById("houseValue").value;
-    var mortgage = document.getElementById("mortgage").value;
-    var creditCardPay = document.getElementById("creditCardPay").value;
-    var carPay = document.getElementById("carPay").value;
-    var studentLoan = document.getElementById("studentLoans").value;
+    var downPay = parseFloat(document.getElementById("downPay").value);
+    var grossMonthlyIncome = parseFloat(document.getElementById("grossIncome").value);
+    var houseValue = parseFloat(document.getElementById("houseValue").value);
+    var mortgage = parseFloat(document.getElementById("mortgage").value);
+    var creditCardPay = parseFloat(document.getElementById("creditCardPay").value);
+    var carPay = parseFloat(document.getElementById("carPay").value);
+    var studentLoan = parseFloat(document.getElementById("studentLoans").value);
 
-    var LTV = LTV(downPay, houseValue);
-    alert(LTV);
+    var LTVResult = LTV(downPay, houseValue);
+    alert(LTVResult);
+
+    var PMIValue = PMI(houseValue);
+
+    var monthlyDebtResult = monthlyDebt(carPay, creditCardPay, mortgage, studentLoan, PMIValue);
+
+    var DTIResult = DTI(monthlyDebtResult, grossMonthlyIncome);
+
+    var FEDTIResult = FEDTI(mortgage, grossMonthlyIncome);
 }
 
 function LTV(downPayment, houseValue) {
-    return downPayment/houseValue;
+    return downPayment / houseValue;
 }
 
 function PMI(houseValue) {
-    return (0.01 * houseValue)/12;
+    return (0.01 * houseValue) / 12;
 }
-
-var PMIValue = PMI(houseValue);
 
 function monthlyDebt(carPay, creditCardPay, mortgage, studentLoan, PMIValue) {
-    return carPay + creditCardPay + mortgage + studentLoan + PMI;
+    return carPay + creditCardPay + mortgage + studentLoan + PMIValue;
 }
 
-var monthlyDebt = monthlyDebt(carPay, creditCardPay, mortgage, studentLoan, PMIValue);
-
 function DTI(monthlyDebt, grossMonthlyIncome) {
-    return monthlyDebt/grossMonthlyIncome;
+    return monthlyDebt / grossMonthlyIncome;
 }
 
 function FEDTI(mortgage, grossMonthlyIncome) {
-    return mortgage/grossMonthlyIncome;
+    return mortgage / grossMonthlyIncome;
 }
