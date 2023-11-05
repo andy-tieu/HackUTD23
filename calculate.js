e45295db9bed99cf317822d0865cb18fa0084333
Andy Tieu <111897924+andy-tieu@users.noreply.github.com>
2023-11-05 01:11:57 +0000

    Attempts to test the js

--- calculate.js
+++ calculate.js
@@ -1,3 +1,14 @@
+document.getElementById("submitButton").addEventListener("click", function () {
+    // Get the input value
+    var grossMonthlyIncome = document.getElementById("grossIncome").value;
+    var houseValue = document.getElementById("houseValue").value;
+    var mortgage = document.getElementById("mortgage").value;
+    var creditCardPay = document.getElementById("creditCardPay").value;
+    var carPay = document.getElementById("carPay").value;
+    var studentLoan = document.getElementById("studentLoans").value;
+    alert("Your mortgage is $" + mortgage);
+});
+
 function LTV(downPayment, houseValue) {
     return downPayment/houseValue;
 }
@@ -6,9 +17,13 @@ function PMI(houseValue) {
     return (0.01 * houseValue)/12;
 }
 
-function monthlyDebt(carPay, creditCardPay, mortgage, studentLoan, PMI) {
+var PMIValue = PMI(houseValue);
+
+function monthlyDebt(carPay, creditCardPay, mortgage, studentLoan, PMIValue) {
     return carPay + creditCardPay + mortgage + studentLoan + PMI;
 }
+
+var monthlyDebt = monthlyDebt(carPay, creditCardPay, mortgage, studentLoan, PMIValue);
 
 function DTI(monthlyDebt, grossMonthlyIncome) {
     return monthlyDebt/grossMonthlyIncome;
